6a708898610bfe92f2ae6e43eb2b3639cd0aa383
Youssef ELBATTAH <youssef.elbattah@sycon.fr>
2025-01-14 15:21:57 +0100

    main build & run

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+
+int sum(int a , int b){
+	return a+b;
+}
 int main()
 {
-	printf("Hello from main");
+	int a = 8, b = 9;
+	int somme = sum(a,b);
+	printf("the sum of %d + %d is %d",a ,b,somme);
 	
 	return 0;
 }
